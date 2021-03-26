9d8ccbf41fdfc52fd2b1b86a17d294489d73b6e7
matthieu101 <matthieu.101@homail.fr>
2021-03-26 16:10:34 +0100

    Song + Organisation

---
+++ js/Component/resizeWindow.js
@@ -0,0 +1,12 @@
+function responsiveWindow(renderer){
+    renderer.setSize( window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    window.addEventListener('resize', function()
+    {
+        var width = window.innerWidth;
+        var height = window.innerHeight;
+        renderer.setSize(width, height);
+        camera.aspect = width /height;
+        camera.updateProjectionMatrix();
+    })
+}
--- js/index.js
+++ js/index.js
@@ -1,71 +1,78 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1 ,1000);
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-window.addEventListener('resize', function()
-{
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    renderer.setSize(width, height);
-    camera.aspect = width /height;
-    camera.updateProjectionMatrix();
-})
+const startButton = document.getElementById( 'startButton' );
+startButton.addEventListener( 'click', init );
 
-document.addEventListener("keydown", onDocumentKeyDown, false);
-function onDocumentKeyDown(event) {
-    var keyCode = event.which;
-    if (keyCode == 39 && pants.position.x >= -0.65) {
-        pants.position.x -= xSpeed;
-    } else if (keyCode == 37 && pants.position.x <= 0.65) {
-        pants.position.x += xSpeed;
-    }
-};
-var xSpeed = 0.05;
+const xSpeed = 0.05;
+const zSpeed = 0.01;
 
-//controls = new THREE.OrbitControls(camera, renderer.domElement);
-//controls.enableDamping = false;
+function init() {
+    const overlay = document.getElementById( 'overlay' );
+	overlay.remove();
 
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
-scene.add( directionalLight );
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1 ,1000);
+    var renderer = new THREE.WebGLRenderer();
+    const listener = new THREE.AudioListener();
+    
+    responsiveWindow(renderer);
 
-const loader = new THREE.ObjectLoader();
-var pants;
-loader.load(
-	"json/scene.json",
+    camera.add( listener );
+    PlaySong(listener);
 
-	// onLoad callback
-	// Here the loaded data is assumed to be an object
-	function ( obj ) {
-        pants  = obj.getObjectByName( "jeans.obj" );
-        obj.position.z = 10;
-        obj.position.y = -1;
-		// Add the loaded object to the scene
-		scene.add( obj );
-	},
-);
-camera.position.y = 0.5;
-camera.rotation.y = Math.PI;
-// game logic
-var update = function()
-{
-    if(pants != undefined && pants.position.z < 19 )
+    document.addEventListener("keydown", onDocumentKeyDown, false);
+    function onDocumentKeyDown(event) {
+        var keyCode = event.which;
+        if (keyCode == 39 && pants.position.x >= -0.65) {
+            pants.position.x -= xSpeed;
+        } else if (keyCode == 37 && pants.position.x <= 0.65) {
+            pants.position.x += xSpeed;
+        }
+    };
+    
+    //controls = new THREE.OrbitControls(camera, renderer.domElement);
+    //controls.enableDamping = false;
+
+    // Light
+    const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+    scene.add( directionalLight );
+
+    const loader = new THREE.ObjectLoader();
+    var pants;
+    loader.load(
+    	"json/scene.json",
+
+    	// onLoad callback
+    	// Here the loaded data is assumed to be an object
+    	function ( obj ) {
+            pants  = obj.getObjectByName( "jeans.obj" );
+            obj.position.z = 10;
+            obj.position.y = -1;
+    		// Add the loaded object to the scene
+    		scene.add( obj );
+    	},
+    );
+    camera.position.y = 0.5;
+    camera.rotation.y = Math.PI;
+    // game logic
+    var update = function()
     {
-        pants.position.z += 0.01;
-        camera.position.z = pants.position.z + 8;
-        console.log(pants.position.x);
-    }
-};
-// draw scene
-var render = function()
-{
-    renderer.render (scene, camera);
-};
-// run game loop
-var GameLoop = function()
-{
-    requestAnimationFrame( GameLoop);
-    update();
-    render();
-};
-GameLoop();
+        if(pants != undefined && pants.position.z < 19 )
+        {
+            pants.position.z += zSpeed;
+            camera.position.z = pants.position.z + 8;
+        }
+    };
+    // draw scene
+    var render = function()
+    {
+        renderer.render (scene, camera);
+    };
+    // run game loop
+    var GameLoop = function()
+    {
+        requestAnimationFrame( GameLoop);
+        update();
+        render();
+    };
+    GameLoop();
+}
+
